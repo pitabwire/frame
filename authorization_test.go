25dac0d5d22a9eef2c5ee4431ccb6dc784924c62
Peter Bwire <bwire517@gmail.com>
2021-04-20 13:37:21 +0300

    Improvements to make tests on ACL more comprehensive

--- authorization_test.go
+++ authorization_test.go
@@ -9,7 +9,6 @@ import (
 func TestAuthorizationControlListWrite(t *testing.T) {
 
 	os.Setenv("KETO_AUTHORIZATION_WRITE_URL", "http://localhost:4467")
-	os.Setenv("KETO_AUTHORIZATION_READ_URL", "http://localhost:4466")
 
 	ctx := context.Background()
 	srv := NewService( "Test Srv")
@@ -20,12 +19,34 @@ func TestAuthorizationControlListWrite(t *testing.T) {
 			t.Errorf("Authorization write was not possible see %v", err)
 	}
 
-	err, access := AuthorizationControlListHasAccess(ctx,  "read", "tested")
-		if err != nil {
-				t.Errorf("Authorization check was not possible see %v", err)
-		}else if !access {
-			t.Errorf("Authorization check was forbidden")
-		}
 
+}
+
+func TestAuthorizationControlListHasAccess(t *testing.T) {
+
+	os.Setenv("KETO_AUTHORIZATION_READ_URL", "http://localhost:4466")
+
+	ctx := context.Background()
+	srv := NewService( "Test Srv")
+	ctx = ToContext(ctx, srv)
+
+	err := AuthorizationControlListWrite(ctx,  "read", "reader")
+	if err != nil {
+		t.Errorf("Authorization write was not possible see %v", err)
+	}
+
+	err, access := AuthorizationControlListHasAccess(ctx,  "read", "reader")
+	if err != nil {
+		t.Errorf("Authorization check was not possible see %v", err)
+	}else if !access {
+		t.Errorf("Authorization check was forbidden")
+	}
+
+	err, access = AuthorizationControlListHasAccess(ctx,  "read", "read-master")
+	if err != nil {
+		t.Errorf("Authorization check was not possible see %v", err)
+	}else if access {
+		t.Errorf("Authorization check was not forbidden yet shouldn't exist")
+	}
 
 }

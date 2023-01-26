93b3b909d3abba8a1e0c1183a791c1fe60decb9f
Peter Bwire <bwire517@gmail.com>
2023-01-26 10:07:11 +0300

    minor bugfix for jwt registration

--- jwt.go
+++ jwt.go
@@ -13,7 +13,7 @@ func (s *Service) RegisterForJwtWithParams(ctx context.Context,
 	oauth2ServiceAdminHost string, clientName string, clientID string, clientSecret string,
 	scope string, audienceList []string, metadata map[string]string) error {
 	oauth2AdminURI := fmt.Sprintf("%s%s", oauth2ServiceAdminHost, "/admin/clients")
-	oauth2AdminIDUri := fmt.Sprintf("%s/%s", oauth2AdminURI, s.name)
+	oauth2AdminIDUri := fmt.Sprintf("%s/%s", oauth2AdminURI, clientID)
 
 	status, _, err := s.InvokeRestService(ctx, http.MethodGet, oauth2AdminIDUri, make(map[string]interface{}), nil)
 	if err != nil {
@@ -53,6 +53,24 @@ func (s *Service) RegisterForJwtWithParams(ctx context.Context,
 			Error(" invalid response from server")
 
 		return errors.New("invalid registration response")
+	}
+	return nil
+}
+
+// UnRegisterForJwt utilizing client id we de register external applications for access token generation
+func (s *Service) UnRegisterForJwt(ctx context.Context,
+	oauth2ServiceAdminHost string, clientID string) error {
+
+	oauth2AdminURI := fmt.Sprintf("%s%s/%s", oauth2ServiceAdminHost, "/admin/clients", clientID)
+
+	status, result, err := s.InvokeRestService(ctx, http.MethodDelete, oauth2AdminURI, make(map[string]interface{}), nil)
+	if err != nil {
+		s.L().WithContext(ctx).
+			WithError(err).
+			WithField("status", status).
+			WithField("result", string(result)).
+			Error(" invalid response from server")
+		return err
 	}
 	return nil
 }

c9640d53866f940fd0f1d6d0bcda863849c6e845
Peter Bwire <bwire517@gmail.com>
2025-06-10 10:49:30 +0300

    minor bugfix

--- datastore.go
+++ datastore.go
@@ -378,7 +378,7 @@ func DatastoreConnectionWithName(ctx context.Context, name string, postgresqlCon
 
 	return func(s *Service) {
 
-		dbQueryLogger := buildDBLogger(s.L(ctx))
+		dbQueryLogger := buildDBLogger(ctx, s)
 
 		cleanedPostgresqlDSN, err := cleanPostgresDSN(postgresqlConnection)
 		if err != nil {
